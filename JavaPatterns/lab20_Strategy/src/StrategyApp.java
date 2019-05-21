d3151614f3537d33897812a334f3ba9b3c2ececc
Max Koroliov <fushiamephit21@gmail.com>
2019-05-21 18:06:13 +0300

    add_Java_Lab20_Strategy

--- JavaPatterns/lab20_Strategy/src/StrategyApp.java
+++ JavaPatterns/lab20_Strategy/src/StrategyApp.java
@@ -1,5 +1,28 @@
 public class StrategyApp {
-    public static void main(String[] args) {
+    public void main(String[] args) {
+        StrategyClient c = new StrategyClient();
+        c.setStrategy(new StrategyA());
         
+        //doSomething...
+    }
+
+    // Context
+    class StrategyClient {
+        Sorting strategy;
+        public StrategyClient() {}
+        public void setStrategy(Sorting strategy) {this.strategy = strategy;}
+        public void executeStrategy(int[] arr) {strategy.sort(arr);}
+    }
+
+    // Strategy
+    interface Sorting {
+        void sort(int[] arr);
+    }
+
+    //Implement Strategy
+    class StrategyA implements Sorting{
+        public void sort(int[] arr) {
+
+        }
     }
 }
