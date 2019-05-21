4ddebf600bfcde037e9b7942db215e19857daa57
Max Koroliov <fushiamephit21@gmail.com>
2019-05-21 17:43:42 +0300

    v.1.2

--- JavaPatterns/lab19_State/src/StateApp.java
+++ JavaPatterns/lab19_State/src/StateApp.java
@@ -1,3 +1,53 @@
 public class StateApp {
-    
+    public static void main(String[] args) {
+        Station dfm = new RadioDFM();
+        Radio radio = new Radio();
+        radio.setStation(dfm);
+        radio.play();
+        radio.nextStation();
+        radio.play();
+    }
+
+    interface Station {
+        void play();
+    }
+
+    static class Radio7 implements Station {
+        public void play() {
+            System.out.println("Радио 7...");
+        }
+    }
+
+    static class RadioDFM implements Station {
+        public void play() {
+            System.out.println("Радио DFM...");
+        }
+    }
+
+    static class VestiFM implements Station {
+        public void play() {
+            System.out.println("Вести FM...");
+        }
+    }
+
+
+    //Context
+    static class Radio{
+        Station station;
+        void setStation(Station st) {station = st;}
+        void nextStation(){
+            if (station instanceof Radio7) {
+                setStation(new RadioDFM());
+            }
+            else if (station instanceof RadioDFM) {
+                setStation(new VestiFM());
+            }
+            else if (station instanceof VestiFM) {
+                setStation(new Radio7());
+            }
+        }
+        void play() {
+            station.play();
+        }
+    }
 }
---
+++ JavaPatterns/lab20_Strategy/src/StrategyApp.java
@@ -0,0 +1,5 @@
+public class StrategyApp {
+    public static void main(String[] args) {
+        
+    }
+}
