6da95d089b3cc14fc1d0049ca48486cfae708514
Max Koroliov <fushiamephit21@gmail.com>
2019-05-29 21:13:31 +0300

    finish_Java_Lab13
    compliet Chain of Responsibility pattern.

--- JavaPatterns/lab13_ChainOfResponsibility/src/ChainOfResponsibilityApp.java
+++ JavaPatterns/lab13_ChainOfResponsibility/src/ChainOfResponsibilityApp.java
@@ -1,32 +1,70 @@
+// Цепочка обов'язків. Рівень доступу.
+
 public class ChainOfResponsibilityApp {
     public static void main(String[] args) {
-        SMSLogger logger0 = new SMSLogger(Level.ERROR);
+        ClientUser user0 = new ClientUser(Level.ONLY_READ);
+        ModeratorUser user1 = new ModeratorUser(Level.EDITING);
+        AdminUser user2 = new AdminUser(Level.ALL);
+        user0.setNext(user1);
+        user1.setNext(user2);
+
+        user0.did("Отримати розклад реклами", Level.ONLY_READ);
+        user0.did("Редагувати розклад реклами", Level.EDITING);
+        user0.did("Видалення розкладу реклами", Level.ALL);
     }
 }
 
 class Level {
-    public static final int ERROR = 1;
-    public static final int DEBUG = 2;
-    public static final int INFO = 3;
+    public static final int ONLY_READ= 1;
+    public static final int EDITING = 2;
+    public static final int ALL = 3;
 }
 
-interface Logger {
-    void writeMessage(String message, int level);
+interface User {
+    void did(String history, int level);
 }
 
-class SMSLogger implements Logger {
-    int priority;
-    public SMSLogger(int priority) {this.priority = priority;}
+class ClientUser implements User {
+    int accessLevel;
+    public ClientUser(int accessLevel) {this.accessLevel = accessLevel;}
 
-    Logger next;
-    public void setNext(Logger next) {this.next = next;}
+    User next;
+    public void setNext(User next) {this.next = next;}
 
-    public void writeMessage(String message, int level) {
-        if (level<=priority) {
-            System.out.println("СМС: " + message);
+    public void did(String history, int level) {
+        if (level<=accessLevel) {
+            System.out.println("Log Клієнта: " + history);
         }
-        if (next==null) {
-            next.writeMessage(message, level);
+        if (next!=null) {
+            next.did(history, level);
+        }
+    }
+}
+
+class ModeratorUser implements User {
+    int accessLevel;
+    public ModeratorUser(int accessLevel) {this.accessLevel = accessLevel;}
+
+    User next;
+    public void setNext(User next) {this.next = next;}
+
+    public void did(String history, int level) {
+        if (level<=accessLevel) {
+            System.out.println("Log Модератора: " + history);
+        }
+        if (next!=null) {
+            next.did(history, level);
+        }
+    }
+}
+
+class AdminUser implements User {
+    int accessLevel;
+    public AdminUser(int accessLevel) {this.accessLevel = accessLevel;}
+
+    public void did(String history, int level) {
+        if (level<=accessLevel) {
+            System.out.println("Log Адміністратора: " + history);
         }
     }
 }
