e980027ee54ab4946a86b1c9bc30fc9fff471a97
Max Koroliov <fushiamephit21@gmail.com>
2019-05-29 20:09:57 +0300

    finish_Java_Lab11
    compliet Flyweight pattern.

--- JavaPatterns/lab11_Flyweight/src/FlyweightApp.java
+++ JavaPatterns/lab11_Flyweight/src/FlyweightApp.java
@@ -1,70 +1,92 @@
+import java.text.MessageFormat;
 import java.util.*;
 
 public class FlyweightApp {
     public static void main(String[] args) {
 
-        ShapeFactory shapeFactory = new ShapeFactory();
+        BroadcastingFactory broadcastingFactory = new BroadcastingFactory();
 
-        List<Shape> shapes = new ArrayList<>();
+        List<Broadcasting> broadcastings = new ArrayList<>();
 
-        shapes.add(shapeFactory.getShape("квадрат"));
-        shapes.add(shapeFactory.getShape("круг"));
-        shapes.add(shapeFactory.getShape("круг"));
-        shapes.add(shapeFactory.getShape("точка"));
-        shapes.add(shapeFactory.getShape("квадрат"));
-        shapes.add(shapeFactory.getShape("круг"));
+        broadcastings.add(broadcastingFactory.getBroadcasting("video"));
+        broadcastings.add(broadcastingFactory.getBroadcasting("advrtismnt"));
+        broadcastings.add(broadcastingFactory.getBroadcasting("advrtismnt"));
+        broadcastings.add(broadcastingFactory.getBroadcasting("pp"));
+        broadcastings.add(broadcastingFactory.getBroadcasting("video"));
+        broadcastings.add(broadcastingFactory.getBroadcasting("pp"));
 
         Random rand = new Random();
-        for (Shape shape : shapes) {
-            int x = rand.nextInt(100);
-            int y = rand.nextInt(100);
-            shape.draw(x,y);
-        }
-    }
-}
-
-interface Shape {
-    void draw(int x, int y);
-}
-
-class Point implements Shape {
-    public void draw(int x, int y) {
-        System.out.println("["+x+", "+y+"] : рисуем точку");
-    }
-}
-
-class Circle implements Shape {
-    int r = 5;
-    public void draw(int x, int y) {
-        System.out.println("["+x+", "+y+"] : рисуем круг радиусом " +r);
-    }
-}
-
-class Square implements Shape {
-    int a=10;
-    public void draw(int x, int y) {
-        System.out.println("["+x+", "+y+"] : рисуем квадрат со стороной "+a);
-    }
-}
-
-class ShapeFactory {
-    private static final Map<String, Shape> shapes = new HashMap<>();
-    public Shape getShape(String shapeName) {
-        Shape shape = shapes.get(shapeName);
-        if (shape==null) {
-            switch(shapeName) {
-                case "круг":
-                    shape = new Circle();
+        for (Broadcasting broadcasting : broadcastings) {
+            int y = (rand.nextInt(3));
+            switch (y) {
+                case 1:
+                    broadcasting.play("Jacobs Coffe");
                     break;
-                case "квадрат":
-                    shape = new Square();
+                case 2:
+                    broadcasting.play("Nescafe Coffe");
                     break;
-                case "точка":
-                    shape = new Point();
+                default:
+                    broadcasting.play("MacCoffe");
                     break;
             }
-            shapes.put(shapeName, shape);
         }
-        return shape;
+    }
+}
+
+interface Broadcasting {
+    void play(String x);
+}
+
+class AdVideo implements Broadcasting {
+    int t = 30;
+    public void play(String x) {
+        template.templ("рекламний відео-ролик", x, t);
+    }
+}
+
+class TVShowWthAdTM implements Broadcasting {
+    int t = 1200;
+    public void play(String x) {
+        template.templ("телешоу з рекламними вставками", x, t);
+    }
+}
+
+class TVShowWthPP implements Broadcasting {
+    int t=1800;
+    public void play(String x) {
+        template.templ("телешоу з продукт-плейсментом", x, t);
+    }
+}
+
+class BroadcastingFactory {
+    private static final Map<String, Broadcasting> broadcastings = new HashMap<>();
+    public Broadcasting getBroadcasting(String broadcastingName) {
+        Broadcasting broadcasting = broadcastings.get(broadcastingName);
+        if (broadcasting==null) {
+            switch(broadcastingName) {
+                case "video":
+                    broadcasting = new AdVideo();
+                    break;
+                case "advrtismnt":
+                    broadcasting = new TVShowWthAdTM();
+                    break;
+                case "pp":
+                    broadcasting = new TVShowWthPP();
+                    break;
+            }
+            broadcastings.put(broadcastingName, broadcasting);
+        }
+        return broadcasting;
+    }
+}
+
+
+//========================================================
+class template {
+    public static void templ(String a, String b, int c) {
+        System.out.println("Відтворюється: "+a);
+        System.out.println("Спонсор: "+b);
+        System.out.println("Тривалість: "+c+" секунд");
+        System.out.println("------------------------------");
     }
 }
